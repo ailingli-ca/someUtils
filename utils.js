6823567a8e11b3aa5961f6143e20f6fe52e89851
Ailing Li <alofe9859@gmail.com>
2022-06-03 01:24:31 +0000

    added a power function

--- utils.js
+++ utils.js
@@ -1,10 +1,15 @@
  function product (num1, num2) {
      return num1, num2;
-
  }
  console.log(product(1,2));
 
  function dividend (num1, num2) {
-     return num1 /num2
+     return num1 /num2;
  }
  
+ function power (n1, n2) {
+     return n1 ** n2;
+ }
+
+
+
